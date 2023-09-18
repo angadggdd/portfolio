41b49126e33df0ff89af120be8e7e7f069a55af6
Angad <angadgpb09876@gmail.com>
2023-09-18 17:43:06 +0530

    initial commit

--- folio.js
+++ folio.js
@@ -5,3 +5,13 @@ var typed = new Typed('.text', {
     backDelay: 1000,
     loop: true
 });
+
+// scroll sections
+
+let sections = document.querySelector('section');
+
+window.onscroll = ()=>{
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky' , window.scrollY > 100);
+}
